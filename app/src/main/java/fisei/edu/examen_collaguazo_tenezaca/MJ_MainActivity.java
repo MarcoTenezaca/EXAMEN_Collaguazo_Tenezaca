8c7e1c8b80f16d981c9aa8b6c5d02d4c87cdc2c7
MarcoTenezaca001 <tnzmarko70@gmail.com>
2022-05-30 17:48:50 -0500

    commit 12

---
+++ app/src/main/java/fisei/edu/examen_collaguazo_tenezaca/MJ_MainActivity.java
@@ -0,0 +1,14 @@
+package fisei.edu.examen_collaguazo_tenezaca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MJ_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mj_main);
+    }
+}
