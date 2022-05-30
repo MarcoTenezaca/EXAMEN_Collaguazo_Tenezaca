4f57c4eb703f1e60d3e0fb55e95b6a350a612193
MarcoTenezaca001 <tnzmarko70@gmail.com>
2022-05-30 18:48:11 -0500

    commIT

--- app/src/main/java/fisei/edu/examen_collaguazo_tenezaca/MJ_MainActivity.java
+++ app/src/main/java/fisei/edu/examen_collaguazo_tenezaca/MJ_MainActivity.java
@@ -2,7 +2,9 @@ package fisei.edu.examen_collaguazo_tenezaca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MJ_MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MJ_MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mj_main);
+    }
+    public void onClick(View view) {
+    Intent intent = new Intent(MJ_MainActivity.this ,MJ_MainActivity_SEGUNDO.class);
+        startActivity(intent);
     }
 }
